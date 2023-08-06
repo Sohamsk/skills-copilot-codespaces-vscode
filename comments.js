11188917b3d09293d6e938c829834eac24d43f7c
Soham Sunil Kudalkar <80628198+Sohamsk@users.noreply.github.com>
2023-08-06 13:03:13 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,10 @@
+// Create web server with NodeJS
+Path: server.js
+var http = require('http');
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, {
+        "Content-Type": "text/plain"
+    });
+    res.end("Hello World!");
+});
+server.listen(3000)
